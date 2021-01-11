d8152c86088ded998530e8ee2ed06a17f6391cef
Dekic2003 <Dekic2003@users.noreply.github.com>
2021-01-11 21:35:18 +0100

    :boom:Added register and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,47 @@
 const router=require('express').Router();
 const User = require('../models/user.model');
-const {registerValidation}=require('../validation/userValidation')
+const bcrypt  = require('bcryptjs')
+const {registerValidation,loginValidation}=require('../validation/userValidation');
 
 router.post('/register', async (req,res)=>{
-    console.log(registerValidation(req.body));
+
+    if(!req.body)return res.status(400).send('Req body missing');
+    const {error}=registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const emailExist = await User.findOne({email:req.body.email});
+    if (emailExist) return res.send('Mail already in use');
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass=await bcrypt.hash( req.body.password,salt);
+
+
+
     const user = new User({
         name:req.body.name,
         email:req.body.email,
-        password:req.body.password
+        password:hashedPass
     })
-    res.send('beeee')
     try {
-        /*const savedUser = await user.save();
-        res.send(savedUser);*/
+        const savedUser = await user.save();
+        res.send(savedUser._id);
     }catch (err) {
         res.status(400).send(err)
     }
 })
+
+router.post('/login', async (req,res)=>{
+    if(!req.body)return res.status(400).send('Req body missing');
+    const {error}=loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const user = await User.findOne({email:req.body.email});
+    if (!user) return res.status(400).send('Mail incorrect');
+    const validPass=await bcrypt.compare(req.body.password,user.password);
+    if(!validPass) return res.status(400).send('Password incorrect');
+
+    res.send('Logged in');
+
+})
+
 
 
 module.exports=router;
--- validation/userValidation.js
+++ validation/userValidation.js
@@ -10,5 +10,15 @@ const registerValidation=(data)=>{
     })
     return schema.validate(data);
 }
+const loginValidation=(data)=>{
+    const schema=Joi.object({
+        email:Joi.string().email().required(),
+        password:Joi.string().min(6).max(1024).required()
+
+    })
+    return schema.validate(data);
+}
 
 module.exports.registerValidation=registerValidation;
+module.exports.loginValidation=loginValidation;
+
