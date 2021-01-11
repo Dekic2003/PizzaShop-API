fb8d291f5b8dd2bf0681fd5810889bba81351b96
Dekic2003 <Dekic2003@users.noreply.github.com>
2021-01-11 21:46:41 +0100

    :boom:Added register and login

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ router.post('/register', async (req,res)=>{
     const {error}=registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     const emailExist = await User.findOne({email:req.body.email});
+
     if (emailExist) return res.send('Mail already in use');
 
     const salt = await bcrypt.genSalt(10);
